8655cba57dbd44ed8d29211dae5ff2c7cb6547d1
Elhadji NGOM <ngomel.ehn@gmail.com>
2022-11-30 12:38:01 +0100

    bg update

--- website_app.py
+++ website_app.py
@@ -138,7 +138,7 @@ def main():
                              icons=['house', 'bar-chart-line', 'app-indicator', 'person lines fill'],
                              menu_icon="list", default_index=0,
                              styles={
-            "container": {"padding": "5!important", "background-color": "#fafafa"}, # "#fafafa"
+            "container": {"padding": "5!important", "background-color": "#999999"}, # "#fafafa"
             "icon": {"color": "blue", "font-size": "25px"},
             "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
             "nav-link-selected": {"background-color": "#24A608"},

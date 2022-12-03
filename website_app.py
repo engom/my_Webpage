8658ae4ce4c2a01e202660a8ed5f2c4342711ab7
Elhadji NGOM <ngomel.ehn@gmail.com>
2022-12-03 16:41:56 +0100

    menu

--- website_app.py
+++ website_app.py
@@ -124,6 +124,7 @@ embed_component = {'linkedin':"""
 def main():
     # -------- MENU SIDE BAR ----------
     with st.sidebar:
+        ds_img = load_image(r'.images/data_science.png')
         choose = option_menu("Page Menu", ["About", "Projects", "Apps", "Contact"],
                              icons=['house', 'bar-chart-line', 'app-indicator', 'person lines fill'],
                              menu_icon="list", default_index=0,
@@ -134,6 +135,7 @@ def main():
             "nav-link-selected": {"background-color": "#24A608"},
         }
         )
+        st.image(ds_img, width=300)
 
     logo = load_image(r'./images/el_00.png')
     if choose == "About":

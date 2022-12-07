ad7ac323064229f85f8e1405df5adb999a786f8f
Elhadji NGOM <ngomel.ehn@gmail.com>
2022-12-07 13:55:25 +0100

    linkedin

--- website_app.py
+++ website_app.py
@@ -214,8 +214,8 @@ def main():
             with st.container():
                 col1, ce, col2 = st.columns([0.4, 0.2, 0.4])
                 with col1:
-                    st.markdown(f"""<p style='text-align: center;'>
-                                \t {embed_component['linkedin_p']}</p>""",
+                    st.markdown(f"""<h3 style='text-align: center;'>
+                                {embed_component['linkedin_p']}</h3>""",
                                 unsafe_allow_html=True)
                     components.html(embed_component['linkedin'],
                                 height=300)
